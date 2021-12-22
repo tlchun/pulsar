5a3e42b3ba11eb99552c072f23263cbb32fa6100
tlchun <513036862@qq.com>
2021-12-22 10:59:34 +0800

    Update pulsar_producers.erl

--- src/pulsar_producers.erl
+++ src/pulsar_producers.erl
@@ -10,64 +10,68 @@
 -author("root").
 
 -export([start_supervised/3, stop_supervised/1, start_link/3]).
-
 -export([pick_producer/2]).
 
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2]).
-
--record(state,
-{topic,
-  client_id,
-  workers,
-  partitions,
-  producer_opts,
-  producers = #{}}).
+-record(state, {topic, client_id, workers, partitions, producer_opts, producers = #{}}).
 
 
 start_supervised(ClientId, Topic, ProducerOpts) ->
-%%  开启生产者
   {ok, Pid} = pulsar_producers_sup:ensure_present(ClientId, Topic, ProducerOpts),
   {Partitions, Workers} = gen_server:call(Pid, get_workers, infinity),
-  {ok,
-    #{client => ClientId, topic => Topic,
-      workers => Workers, partitions => Partitions,
-      strategy => maps:get(strategy, ProducerOpts, random)}}.
-%% 停止
+  {ok, #{client => ClientId, topic => Topic, workers => Workers, partitions => Partitions, strategy => maps:get(strategy, ProducerOpts, random)}}.
+
 stop_supervised(#{client := ClientId, workers := Workers}) ->
   pulsar_producers_sup:ensure_absence(ClientId, Workers).
-%% 启动
+
 start_link(ClientId, Topic, ProducerOpts) ->
   gen_server:start_link({local, get_name(ProducerOpts)}, pulsar_producers, [ClientId, Topic, ProducerOpts], []).
 
-%% 选择生产者
-pick_producer(#{workers := Workers, partitions := Partitions, strategy := Strategy}, Batch) ->
+pick_producer(#{workers := Workers,
+  partitions := Partitions, strategy := Strategy},
+    Batch) ->
   Partition = pick_partition(Partitions, Strategy, Batch),
-  do_pick_producer(Strategy, Partition, Partitions, Workers).
+  do_pick_producer(Strategy,
+    Partition,
+    Partitions,
+    Workers).
 
-do_pick_producer(Strategy, Partition, Partitions, Workers) ->
+do_pick_producer(Strategy, Partition, Partitions,
+    Workers) ->
   Pid = lookup_producer(Workers, Partition),
   case is_pid(Pid) andalso is_process_alive(Pid) of
     true -> {Partition, Pid};
     false when Strategy =:= random ->
       pick_next_alive(Workers, Partition, Partitions);
     false when Strategy =:= roundrobin ->
-      R = {Partition, Pid} = pick_next_alive(Workers, Partition, Partitions),
-      _ = put(pulsar_roundrobin, (Partition + 1) rem Partitions),
+      R = {Partition, Pid} = pick_next_alive(Workers,
+        Partition,
+        Partitions),
+      _ = put(pulsar_roundrobin,
+        (Partition + 1) rem Partitions),
       R;
     false -> erlang:error({producer_down, Pid})
   end.
 
 pick_next_alive(Workers, Partition, Partitions) ->
-  pick_next_alive(Workers, (Partition + 1) rem Partitions, Partitions, _Tried = 1).
+  pick_next_alive(Workers,
+    (Partition + 1) rem Partitions,
+    Partitions,
+    _Tried = 1).
 
-pick_next_alive(_Workers, _Partition, Partitions, Partitions) ->
+pick_next_alive(_Workers, _Partition, Partitions,
+    Partitions) ->
   erlang:error(all_producers_down);
-pick_next_alive(Workers, Partition, Partitions, Tried) ->
+pick_next_alive(Workers, Partition, Partitions,
+    Tried) ->
   Pid = lookup_producer(Workers, Partition),
   case is_alive(Pid) of
     true -> {Partition, Pid};
     false ->
-      pick_next_alive(Workers, (Partition + 1) rem Partitions, Partitions, Tried + 1)
+      pick_next_alive(Workers,
+        (Partition + 1) rem Partitions,
+        Partitions,
+        Tried + 1)
   end.
 
 is_alive(Pid) ->
@@ -75,7 +79,8 @@ is_alive(Pid) ->
 
 lookup_producer(#{workers := Workers}, Partition) ->
   lookup_producer(Workers, Partition);
-lookup_producer(Workers, Partition) when is_map(Workers) ->
+lookup_producer(Workers, Partition)
+  when is_map(Workers) ->
   maps:get(Partition, Workers);
 lookup_producer(Workers, Partition) ->
   case ets:lookup(Workers, Partition) of
@@ -90,8 +95,11 @@ pick_partition(Partitions, roundrobin, _) ->
                 undefined -> 0;
                 Number -> Number
               end,
-  _ = put(pulsar_roundrobin, (Partition + 1) rem Partitions), Partition;
-pick_partition(Partitions, first_key_dispatch,[#{key := Key} | _]) ->
+  _ = put(pulsar_roundrobin,
+    (Partition + 1) rem Partitions),
+  Partition;
+pick_partition(Partitions, first_key_dispatch,
+    [#{key := Key} | _]) ->
   murmerl3:hash_32(Key) rem Partitions.
 
 init([ClientId, Topic, ProducerOpts]) ->
@@ -99,27 +107,50 @@ init([ClientId, Topic, ProducerOpts]) ->
   {ok,
     #state{topic = Topic, client_id = ClientId,
       producer_opts = ProducerOpts,
-      workers = ets:new(get_name(ProducerOpts), [protected, named_table, {read_concurrency, true}])},
+      workers =
+      ets:new(get_name(ProducerOpts),
+        [protected, named_table, {read_concurrency, true}])},
     0}.
 
-handle_call(get_workers, _From, State = #state{workers = Workers, partitions = Partitions}) ->
+handle_call(get_workers, _From,
+    State = #state{workers = Workers,
+      partitions = Partitions}) ->
   {reply, {Partitions, Workers}, State};
 handle_call(_Call, _From, State) ->
   {reply, {error, unknown_call}, State}.
 
 handle_cast(_Cast, State) -> {noreply, State}.
 
-handle_info(timeout, State = #state{client_id = ClientId, topic = Topic, workers = Workers, producer_opts = ProducerOpts, producers = Producers}) ->
+handle_info(timeout,
+    State = #state{client_id = ClientId, topic = Topic,
+      workers = Workers, producer_opts = ProducerOpts,
+      producers = Producers}) ->
   case pulsar_client_sup:find_client(ClientId) of
     {ok, Pid} ->
-      {_, Partitions} = pulsar_client:get_topic_metadata(Pid, Topic),
-      PartitionTopics = create_partition_topic(Topic, Partitions),
-      NewProducers = lists:foldl(fun ({PartitionTopic, Partition}, Acc) ->
-        start_producer(Pid, Partition, PartitionTopic, ProducerOpts, Workers, Acc) end, Producers, PartitionTopics),
-      {noreply, State#state{partitions = length(PartitionTopics), producers = NewProducers}};
+      {_, Partitions} = pulsar_client:get_topic_metadata(Pid,
+        Topic),
+      PartitionTopics = create_partition_topic(Topic,
+        Partitions),
+      NewProducers = lists:foldl(fun ({PartitionTopic,
+        Partition},
+          Acc) ->
+        start_producer(Pid,
+          Partition,
+          PartitionTopic,
+          ProducerOpts,
+          Workers,
+          Acc)
+                                 end,
+        Producers,
+        PartitionTopics),
+      {noreply,
+        State#state{partitions = length(PartitionTopics),
+          producers = NewProducers}};
     {error, Reason} -> {stop, {shutdown, Reason}, State}
   end;
-handle_info({'EXIT', Pid, _Error}, State = #state{workers = Workers, producers = Producers}) ->
+handle_info({'EXIT', Pid, _Error},
+    State = #state{workers = Workers,
+      producers = Producers}) ->
   case maps:get(Pid, Producers, undefined) of
     undefined ->
       log_error("Not find Pid:~p producer", [Pid]),
@@ -127,12 +158,23 @@ handle_info({'EXIT', Pid, _Error}, State = #state{workers = Workers, producers =
     {Partition, PartitionTopic} ->
       ets:delete(Workers, Partition),
       self() ! {restart_producer, Partition, PartitionTopic},
-      {noreply, State#state{producers = maps:remove(Pid, Producers)}}
+      {noreply,
+        State#state{producers = maps:remove(Pid, Producers)}}
   end;
-handle_info({restart_producer, Partition, PartitionTopic}, State = #state{client_id = ClientId, producers = Producers, workers = Workers, producer_opts = ProducerOpts}) ->
+handle_info({restart_producer,
+  Partition,
+  PartitionTopic},
+    State = #state{client_id = ClientId,
+      producers = Producers, workers = Workers,
+      producer_opts = ProducerOpts}) ->
   case pulsar_client_sup:find_client(ClientId) of
     {ok, Pid} ->
-      NewProducers = start_producer(Pid, Partition, PartitionTopic, ProducerOpts, Workers, Producers),
+      NewProducers = start_producer(Pid,
+        Partition,
+        PartitionTopic,
+        ProducerOpts,
+        Workers,
+        Producers),
       {noreply, State#state{producers = NewProducers}};
     {error, Reason} -> {stop, {shutdown, Reason}, State}
   end;
@@ -144,11 +186,13 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
 terminate(_, _St) -> ok.
 
-%% 分区主题
 create_partition_topic(Topic, 0) -> [{Topic, 0}];
-
 create_partition_topic(Topic, Partitions) ->
-  lists:map(fun (Partition) -> {lists:concat([Topic, "-partition-", Partition]), Partition} end, lists:seq(0, Partitions - 1)).
+  lists:map(fun (Partition) ->
+    {lists:concat([Topic, "-partition-", Partition]),
+      Partition}
+            end,
+    lists:seq(0, Partitions - 1)).
 
 get_name(ProducerOpts) ->
   maps:get(name, ProducerOpts, pulsar_producers).
@@ -156,14 +200,22 @@ get_name(ProducerOpts) ->
 log_error(Fmt, Args) ->
   error_logger:error_msg(Fmt, Args).
 
-start_producer(Pid, Partition, PartitionTopic, ProducerOpts, Workers, Producers) ->
-  try BrokerServiceUrl = pulsar_client:lookup_topic(Pid, PartitionTopic),
-  {ok, Producer} = pulsar_producer:start_link(PartitionTopic, BrokerServiceUrl, ProducerOpts),
+start_producer(Pid, Partition, PartitionTopic,
+    ProducerOpts, Workers, Producers) ->
+  try BrokerServiceUrl = pulsar_client:lookup_topic(Pid,
+    PartitionTopic),
+  {ok, Producer} =
+    pulsar_producer:start_link(PartitionTopic,
+      BrokerServiceUrl,
+      ProducerOpts),
   ets:insert(Workers, {Partition, Producer}),
-  maps:put(Producer, {Partition, PartitionTopic}, Producers)
+  maps:put(Producer,
+    {Partition, PartitionTopic},
+    Producers)
   catch
     Error:Reason:Stacktrace ->
-      log_error("Start producer: ~p, ~p", [Error, {Reason, Stacktrace}]),
+      log_error("Start producer: ~p, ~p",
+        [Error, {Reason, Stacktrace}]),
       self() ! {restart_producer, Partition, PartitionTopic},
       Producers
   end.
