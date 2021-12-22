f920dbedfac83739fdb811b6f60286c6ea32c742
tlchun <513036862@qq.com>
2021-12-22 10:57:51 +0800

    Update pulsar_consumers_sup.erl

--- src/pulsar_consumers_sup.erl
+++ src/pulsar_consumers_sup.erl
@@ -9,29 +9,23 @@
 -module(pulsar_consumers_sup).
 -author("root").
 
+
 -behaviour(supervisor).
-
 -export([start_link/0, init/1]).
-
 -export([ensure_present/3, ensure_absence/2]).
 
 
 start_link() ->
-  supervisor:start_link({local, pulsar_consumers_sup},
-    pulsar_consumers_sup,
-    []).
+  supervisor:start_link({local, pulsar_consumers_sup}, pulsar_consumers_sup, []).
 
 init([]) ->
-  SupFlags = #{strategy => one_for_one, intensity => 10,
-    period => 5},
+  SupFlags = #{strategy => one_for_one, intensity => 10, period => 5},
   Children = [],
   {ok, {SupFlags, Children}}.
 
 ensure_present(ClientId, Topic, ConsumerOpts) ->
   ChildSpec = child_spec(ClientId, Topic, ConsumerOpts),
-  case supervisor:start_child(pulsar_consumers_sup,
-    ChildSpec)
-  of
+  case supervisor:start_child(pulsar_consumers_sup, ChildSpec) of
     {ok, Pid} -> {ok, Pid};
     {error, {already_started, Pid}} -> {ok, Pid};
     {error, {{already_started, Pid}, _}} -> {ok, Pid};
@@ -41,16 +35,14 @@ ensure_present(ClientId, Topic, ConsumerOpts) ->
 ensure_absence(ClientId, Name) ->
   Id = {ClientId, Name},
   case supervisor:terminate_child(pulsar_consumers_sup, Id) of
-    ok -> ok = supervisor:delete_child(pulsar_consumers_sup, Id);
+    ok ->
+      ok = supervisor:delete_child(pulsar_consumers_sup, Id);
     {error, not_found} -> ok
   end.
 
 child_spec(ClientId, Topic, ConsumerOpts) ->
   #{id => {ClientId, get_name(ConsumerOpts)},
-    start =>
-    {pulsar_consumers,
-      start_link,
-      [ClientId, Topic, ConsumerOpts]},
+    start => {pulsar_consumers, start_link, [ClientId, Topic, ConsumerOpts]},
     restart => transient, type => worker,
     modules => [pulsar_consumers]}.
 
